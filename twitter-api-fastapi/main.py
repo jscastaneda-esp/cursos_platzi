9c16c6bb8422d100940b71717f408ef26d0f7ae4
Jonathan Castañeda <jcaatanedaesp@gmail.com>
2022-03-05 10:26:47 -0500

    Model users Twitter API

---
+++ twitter-api-fastapi/main.py
@@ -0,0 +1,46 @@
+# Python
+from datetime import date
+from uuid import UUID
+
+# Pydantic
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    Field
+)
+
+# FastAPI
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# Models
+class BaseUser(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+
+class UserLogin(BaseUser):
+    password: str = Field(
+        default=...,
+        min_length=8
+    )
+
+class User(BaseUser):
+    first_name: str = Field(
+        default=...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        default=...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: date | None = Field(None)
+
+
+@app.get("/")
+def home():
+    return {"Twitter API": "Working!"}
